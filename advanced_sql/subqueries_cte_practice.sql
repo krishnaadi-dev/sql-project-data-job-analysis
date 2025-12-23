761232ad47f010074b336c4fe5b7d3bfcbcfb2ba
krishnaadi-dev <250351249+krishnaadi-dev@users.noreply.github.com>
2025-12-23 19:38:12 +0800

    subquery qte practice

---
+++ advanced_sql/subqueries_cte_practice.sql
@@ -0,0 +1,27 @@
+-- Practice 1
+-- Identify the top 5 skills that are most frequently mentioned in job postings. Use a subquery to
+-- find the skill IDs with the highest counts in the skills_job_dim table and then join this result
+-- with the skills_dim table to get the skill names.
+SELECT
+    sd.skills AS skill_name
+FROM
+    skills_dim AS sd
+JOIN (
+    SELECT
+        skill_id
+    FROM
+        skills_job_dim
+    GROUP BY
+        skill_id
+    ORDER BY
+        COUNT(job_id) DESC
+    LIMIT 5
+) AS top_skills
+    ON sd.skill_id = top_skills.skill_id;
+
+-- Practice 2
+-- Determine the size category ('Small', 'Medium', or 'Large') for each company by first identifying
+-- the number of job postings they have. Use a subquery to calculate the total job postings per
+-- company. A company is considered 'Small' if it has less than 10 job postings, 'Medium' if the
+-- number of job postings is between 10 and 50, and 'Large' if it has more than 50 job postings.
+-- Implement a subquery to aggregate job counts per company before classifying them based on size.
